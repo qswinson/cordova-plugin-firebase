6d7d6460f916408ab15f77a00cca80c8bee41ad4
Quenby Mitchell <quenby@thedailynumber.com>
2018-04-02 17:41:47 -0500

    setBadgeNumber must be run on main thread

--- src/ios/FirebasePlugin.m
+++ src/ios/FirebasePlugin.m
@@ -158,13 +158,10 @@ static FirebasePlugin *firebasePlugin;
 
 - (void)setBadgeNumber:(CDVInvokedUrlCommand *)command {
     int number = [[command.arguments objectAtIndex:0] intValue];
+    [[UIApplication sharedApplication] setApplicationIconBadgeNumber:number];
 
-    [self.commandDelegate runInBackground:^{
-        [[UIApplication sharedApplication] setApplicationIconBadgeNumber:number];
-
-        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    }];
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void)getBadgeNumber:(CDVInvokedUrlCommand *)command {
